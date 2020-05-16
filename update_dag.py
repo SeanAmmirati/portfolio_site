29ed044272d426d744995ef8d878a17f881ff1e4
Sean Ammirati <sammirati@statsworks.info>
2020-05-16 16:43:51 -0400

    adding kw args

--- update_dag.py
+++ update_dag.py
@@ -98,8 +98,8 @@ dag = DAG('update_portfolio',
           schedule_interval='0 12 * * *',
           start_date=datetime(2020, 5, 16), catchup=False)
 
-update_html = PythonOperator(update_webpage, dag=dag)
+update_html = PythonOperator(python_callable=update_webpage, dag=dag)
 commit_dag = BashOperator(
-    'cd /root/airflow/dags/portfolio && git add . && git commit -m "update based on new statsworks entry" && git push', dag=dag)
+    bash_command='cd /root/airflow/dags/portfolio && git add . && git commit -m "update based on new statsworks entry" && git push', dag=dag)
 
 update_html >> commit_dag
