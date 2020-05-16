6c370e64d8e5c018c6b9e001343efa63421c071e
Sean Ammirati <sammirati@statsworks.info>
2020-05-16 16:12:09 -0400

    dag initialization

---
+++ update_dag.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from generate_blogs import update_webpage
+from datetime import datetime
+
+dag = DAG('update_portfolio',
+          description='Updates Website portion of portfolio with most recent posts',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2020, 5, 16), catchup=False)
+
+update_html = PythonOperator(update_webpage, dag=dag)
+commit_dag = BashOperator(
+    'git add . && git commit -m "update based on new statsworks entry" && git push', dag=dag)
+
+update_html >> commit_dag
