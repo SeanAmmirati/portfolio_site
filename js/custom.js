34ff3e6574a8a95abae4ac201bf4de6d573d37eb
Sean Ammirati <ammirati.sean@gmail.com>
2024-03-01 17:32:04 -0800

    Make Age a Javascript fn to avoid having to update it

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+function calculateAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday;
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+document.getElementById('age_field').innerHTML = calculateAge(Date.parse("1994-01-01"));
